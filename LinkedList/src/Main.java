ef9ce6752f18e589809b64fea641424113aedb58
Chaitanya Shrivastava <81333713+cs-cse@users.noreply.github.com>
2021-05-15 21:11:56 +0530

    Custom

---
+++ LinkedList/src/Main.java
@@ -0,0 +1,31 @@
+
+
+public class Main {
+
+//    public static void main(String[] args) {
+//        Customer customer=new Customer("Chaitanya",100);
+//        Customer anotherCustomer;
+//        anotherCustomer=customer;
+//        anotherCustomer.setBalance(12.34);
+//        System.out.println("Balance for "+ customer.getName() + " is " +
+//                customer.getBalance() );
+//
+//        ArrayList<Integer> intList=new ArrayList<Integer>();
+//        intList.add(23);
+//        intList.add(230);
+//        intList.add(213);
+//        for(int i=0;i<intList.size();i++){
+//            System.out.println("Value at "+ i + " is "+intList.get(i));
+//        }
+//        System.out.println("**********");
+//intList.add(2,3434);
+//
+//        for(int i=0;i<intList.size();i++){
+//            System.out.println("Value at "+ i + " is "+intList.get(i));
+//        } System.out.println("**********");
+//        intList.add(3434);
+//        for(int i=0;i<intList.size();i++){
+//            System.out.println("Value at "+ i + " is "+intList.get(i));
+//        } System.out.println("**********");
+//    }
+}
